4786c17adab5bb61e3f212fb25b07cb3bbc2cd43
Fauroro <151984072+Fauroro@users.noreply.github.com>
2024-02-25 11:24:25 -0500

    feat: :white_check_mark: Add comments
    Add comments of js functions in code

--- js/app.js
+++ js/app.js
@@ -1,12 +1,18 @@
+// LLamado de elementos del DOM por ID
 const botonInicio = document.getElementById('boton-inicio');
 const botonReinicio = document.getElementById('boton-reinicio');
 const cronometro = document.getElementById('cronometro')
 
+// Definicion de variables para el cronometro
 let [horas,minutos,segundos] = [0,0,0];
 
+// estado e intervalo del tiempo transcurrido del cronometro
 let tiempoTranscurrido;
 let estado = 'pausa';
 
+// funcion que denota la logica del funcionamiento del cronometro; tambien actualiza el texto del cronometro en el  HTML
+// 1. se valida que trancurrieron 60 segundos para agregar un minuto y reiniciar el contador de segundos
+// 2. se valida que trancurrieron 60 minutos para agregar una hora y reiniciar el contador de minutos
 const cronometroAndando = () =>{
   segundos++;
   if (segundos===60) {
@@ -26,8 +32,10 @@ const cronometroAndando = () =>{
   cronometro.innerText = `${formatoHoras}:${formatoMinutos}:${formatoSegundos}`;
 }
 
+// Funcion encargada de asignar el formato agregando un 0 antes del numero si se cuenta solo con un digito en el contador
 const formato = (unidad) => unidad < 10 ? '0' + unidad : unidad;
 
+// Funcion asociada al evento click con el boton de inicio que inicia o pausa el cronometro segun su estado actual
 botonInicio.addEventListener('click',function() {
   if (estado==='pausa') {
     tiempoTranscurrido = window.setInterval(cronometroAndando,1000);
@@ -44,6 +52,7 @@ botonInicio.addEventListener('click',function() {
   }
 });
 
+// Funcion asociada el evento click con el boton de reiniciar que reinicia el formato, valores y estado del cronometro
 botonReinicio.addEventListener('click',function(){
   window.clearInterval(tiempoTranscurrido);
   [horas,minutos,segundos] = [0,0,0];
